c1c5b9766882baae63a6e458632aa1336fee3cf0
Philip Kendall <philip@shadowmagic.org.uk>
2017-12-05 19:14:34 +0000

    Day 5, both parts.

---
+++ 05/05-1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+
+with open(sys.argv[1]) as input_file:
+    instructions = []
+    for line in input_file:
+        instructions.append(int(line))
+
+pc = 0
+count = 0
+while pc < len(instructions):
+    count += 1
+    jump = instructions[pc]
+    instructions[pc] += 1
+    pc += jump
+
+print(count)
